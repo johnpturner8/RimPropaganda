e6e1791042bcaf99375ad7918a6a91b88f87fb64
johnpturner8 <johnpturner8@gmail.com>
2022-10-24 21:52:15 -0400

    Fix "propoganda" misspellings

--- Source/RimPropaganda/RimPropaganda/Properties/AssemblyInfo.cs
+++ Source/RimPropaganda/RimPropaganda/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RimPropoganda")]
+[assembly: AssemblyTitle("RimPropaganda")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RimPropoganda")]
+[assembly: AssemblyProduct("RimPropaganda")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
