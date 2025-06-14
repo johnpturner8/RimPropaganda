d6fd74ef763cf291ffb4860883b3116050597aac
johnpturner8 <johnpturner8@gmail.com>
2025-06-14 16:47:18 -0400

    Update to 1.6

---
+++ 1.3/Source/RimPropaganda/RimPropaganda/CompPropaganda.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using Verse.AI;
+
+namespace RimPropaganda
+{
+	[StaticConstructorOnStartup]
+	class CompPropaganda : ThingComp
+    {
+		public bool showingPropaganda = false;
+
+		private Texture2D cachedCommandTex;
+
+		public Ideo ideo;
+
+		public float certaintyFactor => Props.certaintyFactor;
+		public bool canChange => Props.canChange;
+
+		public CompProperties_Propaganda Props
+        {
+            get
+            {
+                return (CompProperties_Propaganda)props;
+            }
+        }
+		public override IEnumerable<FloatMenuOption> CompFloatMenuOptions(Pawn selPawn)
+		{
+			if (canChange)
+			{
+				yield return new FloatMenuOption("SetIdeology".Translate(this.parent.Label) + selPawn.Ideo.name, delegate ()
+				{
+				selPawn.jobs.TryTakeOrderedJob(JobMaker.MakeJob(DefDatabase<JobDef>.GetNamed("SetPropagandaIdeology"), this.parent), new JobTag?(JobTag.Misc), false);
+				}, MenuOptionPriority.Default, null, null, 0f, null, null, true, 0);
+				
+				if (showingPropaganda)
+				{
+					yield return new FloatMenuOption("SetPropagandaOff".Translate(this.parent.Label), delegate ()
+					{
+					selPawn.jobs.TryTakeOrderedJob(JobMaker.MakeJob(DefDatabase<JobDef>.GetNamed("SetPropagandaOff"), this.parent), new JobTag?(JobTag.Misc), false);
+					}, MenuOptionPriority.Default, null, null, 0f, null, null, true, 0);
+				}
+			}
+		}
+
+        public override void PostExposeData()
+		{
+			base.PostExposeData();
+			Scribe_References.Look(ref ideo, "propIdeo");
+			Scribe_Values.Look<bool>(ref showingPropaganda, "showingProp");
+		}
+
+		public override string CompInspectStringExtra()
+        {
+			string message = "";
+			if(!canChange && ideo != null)
+            {
+				message += "Propaganda for " + ideo.name + ".";
+			}
+            else if (this.showingPropaganda)
+            {
+				message += "Showing propaganda for " + ideo.name + ".";
+            }
+			return message;
+        }
+		public override void PostPostMake()
+        {
+			if (Props.autoIdeo)
+			{
+				showingPropaganda = true;
+				ideo = Faction.OfPlayer.ideos.PrimaryIdeo;
+			}
+			Log.Message("PostPostMake CompPropaganda");
+		}
+
+	}
+
+	class CompProperties_Propaganda : CompProperties
+	{
+		public bool canChange = true;
+		public float certaintyFactor;
+		public bool autoIdeo = false;
+
+		public CompProperties_Propaganda()
+		{
+			this.compClass = typeof(CompPropaganda);
+		}
+
+	}
+}
---
+++ 1.3/Source/RimPropaganda/RimPropaganda/TelevisionSetPropJobs.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using Unity;
+using Verse.AI;
+
+namespace RimPropaganda
+{
+    class JobDriver_SetPropagandaIdeology : JobDriver
+    {
+        private Thing Broadcaster
+        {
+            get
+            {
+                return base.TargetThingA;
+            }
+        }
+
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return this.pawn.Reserve(this.Broadcaster, this.job, 1, -1, null, errorOnFailed, true);
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            this.FailOnDespawnedNullOrForbidden(TargetIndex.A);
+            yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.Touch);
+            Toil toil = new Toil();
+            toil.initAction = () =>
+            {
+                Broadcaster.TryGetComp<CompPropaganda>().ideo = pawn.ideo.Ideo;
+                Broadcaster.TryGetComp<CompPropaganda>().showingPropaganda = true;
+            };
+            yield return toil;
+            yield break;
+        }
+    }
+    
+    class JobDriver_SetPropagandaOff : JobDriver
+    {
+        private Thing Broadcaster
+        {
+            get
+            {
+                return base.TargetThingA;
+            }
+        }
+
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return this.pawn.Reserve(this.Broadcaster, this.job, 1, -1, null, errorOnFailed);
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            this.FailOnDespawnedNullOrForbidden(TargetIndex.A);
+            yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.Touch);
+            Toil toil = new Toil();
+            toil.initAction = () =>
+            {
+                Broadcaster.TryGetComp<CompPropaganda>().showingPropaganda = false;
+            };
+            yield return toil;
+            yield break;
+        }
+    }
+
+    class WorkGiver_SetPropagandaIdeology : WorkGiver_Scanner
+    {
+        public override Job JobOnThing(Pawn pawn, Thing t, bool forced)
+        {
+            Job job = new Job(DefDatabase<JobDef>.GetNamed("SetPropagandaIdeology"), t);
+            return job;
+        }
+    }
+
+    class WorkGiver_SetPropagandaOff : WorkGiver_Scanner
+    {
+        public override Job JobOnThing(Pawn pawn, Thing t, bool forced)
+        {
+            Job job = new Job(DefDatabase<JobDef>.GetNamed("SetPropagandaOff"), t);
+            return job;
+        }
+    }
+}
